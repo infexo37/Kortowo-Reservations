5ebdd8a4541edc859bdffa4fbbf9d63457a0c6e2
infexo37 <lukasz.popiolek93@gmail.com>
2015-06-10 13:22:40 +0200

    Rezerwacja Obiektów Sportowych
    fix

--- Rezerwacja Obiektów Sportowych/src/test/Rezerwacja.java
+++ Rezerwacja Obiektów Sportowych/src/test/Rezerwacja.java
@@ -1,11 +1,18 @@
 package test;
 
+import java.sql.Date;
+
 import test.Termin;
 
 public class Rezerwacja {
 
 	private int idRezerwacja;
 	private int liczbaUczestnikow;
+	private String nazwaObiektu;
+	private String adresObiektu;
+	private Date dzien;
+	private String odKtorej;
+	private String doKtorej;
 	private Termin termin;
 	
 	public int getIdRezerwacja() {
@@ -25,5 +32,35 @@ public class Rezerwacja {
 	}
 	public void setTermin(Termin termin) {
 		this.termin = termin;
+	}
+	public String getNazwaObiektu() {
+		return nazwaObiektu;
+	}
+	public void setNazwaObiektu(String nazwaObiektu) {
+		this.nazwaObiektu = nazwaObiektu;
+	}
+	public String getAdresObiektu() {
+		return adresObiektu;
+	}
+	public void setAdresObiektu(String adresObiektu) {
+		this.adresObiektu = adresObiektu;
+	}
+	public Date getDzien() {
+		return dzien;
+	}
+	public void setDzien(Date dzien) {
+		this.dzien = dzien;
+	}
+	public String getOdKtorej() {
+		return odKtorej;
+	}
+	public void setOdKtorej(String odKtorej) {
+		this.odKtorej = odKtorej;
+	}
+	public String getDoKtorej() {
+		return doKtorej;
+	}
+	public void setDoKtorej(String doKtorej) {
+		this.doKtorej = doKtorej;
 	}
 }
