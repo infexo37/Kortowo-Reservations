48ef361d24625429db5a2b96d7b180087f7a7874
orgarkadiusz <org.arkadiusz@gmail.com>
2015-06-10 11:10:04 +0200

    to samo

--- Rezerwacja Obiektów Sportowych/src/test/ListaRezerwacji.java
+++ Rezerwacja Obiektów Sportowych/src/test/ListaRezerwacji.java
@@ -12,24 +12,23 @@ public class ListaRezerwacji {
         Connection conn = null;
         public ListaRezerwacji() throws ClassNotFoundException
         {
-                  conn = ConnectionClass.Polacz();
-     
+        	conn = ConnectionClass.Polacz();
         }
        
         public ArrayList<Rezerwacja> getRezerwacje() throws SQLException
         {
-                ArrayList<Rezerwacja> rezerwacje = new ArrayList<Rezerwacja>();
-                Statement pst = null;  
-        ResultSet rs = null;
-        String query = "SELECT obiekty.nazwa, obiekty.adres, termin.dzien, "
+        	ArrayList<Rezerwacja> rezerwacje = new ArrayList<Rezerwacja>();
+            Statement pst = null;  
+            ResultSet rs = null;
+            String query = "SELECT obiekty.nazwa, obiekty.adres, termin.dzien, "
                         + "termin.odKtorej, termin.doKtorej, rezerwacje.liczbaUczestnikow "
                         + "from rezerwacje left join termin on "
                         + "rezerwacje.idTermin = termin.idTermin left join obiekty on "
                         + "termin.idObiekt = obiekty.idObiekt;";
-        pst = conn.createStatement();
-        rs = pst.executeQuery(query);
+            pst = conn.createStatement();
+            rs = pst.executeQuery(query);
        
-        while(rs.next()){
+            while(rs.next()){
                 Rezerwacja rezerwacja = new Rezerwacja();
                 rezerwacja.setNazwaObiektu(rs.getString("obiekty.nazwa"));
                 rezerwacja.setAdresObiektu(rs.getString("obiekty.adres"));
@@ -38,9 +37,7 @@ public class ListaRezerwacji {
                 rezerwacja.setDoKtorej(rs.getString("termin.doKtorej"));
                 rezerwacja.setLiczbaUczestnikow(rs.getInt("rezerwacje.liczbaUczestnikow"));
                 rezerwacje.add(rezerwacja);
-        }
-       
-        return rezerwacje;
+            }	
+            return rezerwacje;
         }
 }
-
